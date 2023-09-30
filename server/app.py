8afe9fa1d422bf29e42b4ee23dc9ec7ee36f8a23
MutaiAllan <MutaiAllan>
2023-09-30 16:47:03 +0300

    Updated seed.py and added more records

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, jsonify, request, redirect, url_for
 from flask_migrate import Migrate
-from models import db
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -10,6 +10,61 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/')
+def home():
+    return 'Welcome to Restaurant Pizzas'
+
+@app.route('/restaurants')
+def restaurants():
+    restaurants_list = []
+    restaurants = Restaurant.query.all()
+    for restaurant in restaurants:
+        restaurant_dict = {
+            "id": restaurant.id,
+            "name": restaurant.name,
+            "address": restaurant.address
+        }
+        restaurants_list.append(restaurant_dict)
+    response_body = restaurants_list
+    response = make_response(jsonify(response_body), 200)
+    return response
+
+@app.route('/restaurants/<int:id>', methods = ["GET", "DELETE"])
+def retaurant_id(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    restaurant_pizzas = RestaurantPizza.query.filter_by(restaurant_id=id).all()
+    if restaurant:
+        if request.method == "GET":
+            pizza_ids = []
+            pizzas_list = []
+            for restaurant_pizza in restaurant_pizzas:
+                pizza_ids.append(restaurant_pizza.restaurant_id)
+            for pizza_id in pizza_ids:
+                pizza = Pizza.query.filter_by(id=pizza_id).first()
+                pizza_dict = {
+                    "id": pizza.id,
+                    "name": pizza.name,
+                    "ingredients": pizza.ingredients
+                }
+                pizzas_list.append(pizza_dict)
+            pizzas = Pizza.query.filter_by()
+            response_body = {
+                "id": restaurant.id,
+                "name": restaurant.name,
+                "address": restaurant.address,
+                "pizzas": pizzas_list
+            }
+            response = make_response(jsonify(response_body), 200)
+        elif request.method == "DELETE":
+            for restuarnt_pizza in restaurant_pizzas:
+                db.session.delete(restaurant_pizza)
+            db.session.delete(restaurant)
+            reponse = make_response('', 401)
+    else:
+        response_body = {"error": "Restaurant not found"}
+        response = make_response(jsonify(response_body), 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/seed.py
+++ server/seed.py
@@ -4,21 +4,32 @@ from models import Restaurant, Pizza, RestaurantPizza
 
 
 with app.app_context():
+    Restaurant.query.delete()
+    Pizza.query.delete()
+    RestaurantPizza.query.delete()
 
-    restaurant1 = Restaurant(name="Sottocasa NYC", address="298 Atlantic Ave, Brooklyn, NY 11201")
-    restaurant2 = Restaurant(name="PizzArte", address="69 W 55th St, New York, NY 10019")
+    restaurant1 = Restaurant(name="Stellato", address="01100, Nairobi")
+    restaurant2 = Restaurant(name="Sottocasa NYC", address="Brooklyn, NY 11201")
+    restaurant3 = Restaurant(name="PizzArte", address="New York, NY 10019")
+    restaurant4 = Restaurant(name="FFG", address="298 Atlantic Ave")
+
 
 
     pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
     pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+    pizza3 = Pizza(name="Vegetarian Pizza", ingredients="Dough, Mozzarella cheese, Bell peppers, Onions, Black olives, Mushrooms,Spinach")
+    pizza4 = Pizza(name="Quattro Formaggi", ingredients="Tomato sauce, Mozzarella cheese, Parmesan cheese, Gorgonzola cheese, Ricotta cheese")
 
-
-    restaurant_pizza1 = RestaurantPizza(price=15, restaurant=restaurant1, pizza=pizza1)
+    restaurant_pizza1 = RestaurantPizza(price=15, restaurant=restaurant1, pizza=pizza4)
     restaurant_pizza2 = RestaurantPizza(price=18, restaurant=restaurant1, pizza=pizza2)
-    restaurant_pizza3 = RestaurantPizza(price=16, restaurant=restaurant2, pizza=pizza1)
+    restaurant_pizza3 = RestaurantPizza(price=12, restaurant=restaurant2, pizza=pizza1)
+    restaurant_pizza4 = RestaurantPizza(price=10, restaurant=restaurant4, pizza=pizza3)
+    restaurant_pizza5 = RestaurantPizza(price=19, restaurant=restaurant2, pizza=pizza3)
+    restaurant_pizza6 = RestaurantPizza(price=11, restaurant=restaurant3, pizza=pizza1)
+    restaurant_pizza7 = RestaurantPizza(price=10, restaurant=restaurant4, pizza=pizza2)
+    restaurant_pizza8 = RestaurantPizza(price=16, restaurant=restaurant2, pizza=pizza1)
 
 
-    db.session.add_all([restaurant1, restaurant2, pizza1, pizza2, restaurant_pizza1, restaurant_pizza2, restaurant_pizza3])
-
+    db.session.add_all([restaurant1, restaurant2, restaurant3, restaurant4, pizza1, pizza2, pizza3, pizza4, restaurant_pizza1, restaurant_pizza2, restaurant_pizza3,  restaurant_pizza4, restaurant_pizza5, restaurant_pizza6, restaurant_pizza7, restaurant_pizza8])
 
     db.session.commit()
